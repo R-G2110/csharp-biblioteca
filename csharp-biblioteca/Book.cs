51c5efcd42454f18800ef7d55a78df2b8063409e
R-G2110 <argeldelacruz86@gmail.com>
2024-04-22 15:03:32 +0200

    [Add]: Document class

---
+++ csharp-biblioteca/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    internal class Book : Document
+    {
+        public int NumberOfPages { get; set; }
+    }
+
+}
