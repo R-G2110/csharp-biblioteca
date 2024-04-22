7ad8d0fdcfb51f5af1fe37ce1df0c37fd26a280a
R-G2110 <argeldelacruz86@gmail.com>
2024-04-22 14:44:37 +0200

    [Add]: Author class

---
+++ csharp-biblioteca/Author.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    internal class Author
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+    }
+
+}
