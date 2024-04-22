79f55735980f32c1c2dcccc59fcce5e6569db716
R-G2110 <argeldelacruz86@gmail.com>
2024-04-22 14:27:29 +0200

    [Add]: User class

---
+++ csharp-biblioteca/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    internal class User
+    {
+        public string LastName { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string TelNumber { get; set; }
+    }
+
+}
